61347f203829088b4460f31d94739ff32d4d8219
kwonyounghye <kyh90721@gmail.com>
2023-09-05 18:17:31 +0900

    0905

---
+++ js/ani.js
@@ -0,0 +1,36 @@
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+window.addEventListener('DOMContentLoaded', loadFn);
+
+function loadFn() {
+    console.log('로딩중');
+    
+    const intro_ani = qs('.intro1');
+    // let ani_txt = intro_ani.innerText;
+    // // 글자 담기 변수
+    let ani_text = '';
+    // // for of문으로 한 글자씩 순회하기
+    for(let x of ani_txt) {
+        ani_text += `<h1 class="intro1">${x}</h1>`;
+        console.log('x:', x);       
+    }
+    
+    
+}
+
+// const qs = x => document.querySelector(x);
+// const qsa = x => document.querySelectorAll(x);
+// window.addEventListener(qs);
+// console.log("x: ", qs);
+// let intro_ani = qsa('.intro1');
+// let ani_txt = intro_ani.innerText;
+// // 글자 담기 변수
+// let ani_text = '';
+// // for of문으로 한 글자씩 순회하기
+// for(let x of ani_txt) {
+//     console.log(x);
+//     ani_text += `<h1 class="intro1">${x}</h1>`;
+// }
+
+// intro_ani.innerHTML = ani_text;
