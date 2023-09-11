81691e843c8631387cd560155329e4ddf2f4eafd
Tom B. Parker <tombap8@hotmail.com>
2023-09-11 18:19:28 +0900

    ㄴㄴ

--- js/ani.js
+++ js/ani.js
@@ -9,12 +9,17 @@ function loadFn() {
     const intro_ani = qs('.intro1');
     // let ani_txt = intro_ani.innerText;
     // // 글자 담기 변수
+    let ani_txt = intro_ani.innerText;
     let ani_text = '';
     // // for of문으로 한 글자씩 순회하기
+    let num = 0;
     for(let x of ani_txt) {
-        ani_text += `<h1 class="intro1">${x}</h1>`;
-        console.log('x:', x);       
+        ani_text += `<span style="animation-delay:${0.1*num}s">${x}</span>`;
+        console.log('x:', x);   
+        num++;    
     }
+
+    intro_ani.innerHTML = ani_text;
     
     
 }
