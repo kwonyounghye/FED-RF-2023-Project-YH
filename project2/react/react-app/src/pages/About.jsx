61cebbab33d55644ef47ff01e48eb6306c220b52
kwonyounghye <kyh90721@gmail.com>
2023-11-30 07:53:55 +0900

    1130

--- project2/react/react-app/src/pages/About.jsx
+++ project2/react/react-app/src/pages/About.jsx
@@ -4,10 +4,16 @@ import "../css/about.css";
 export function About() {
     return (
         <>
-            <div className="about">
-                <div className="intro1">
-                    {/* 이미지1 */}
-                    <img src="../../images/intro1.jpg" width="50%" />
+                <div className="about">
+                    <div className="intro1">
+                        {/* 이미지1 */}
+                        <img src="../../images/intro1.jpg" width="50%" />
+                    </div>
+                    <div className="contant1">
+                        pomme
+                        <br />
+                        사과
+                    </div>
                 </div>
                 {/* 위치 고정 설명 */}
                 <div className="introduce">
@@ -17,11 +23,17 @@ export function About() {
                         <li>폼베르는 매일 아침 유기농밀가루, 천연발효종, 친환경 식재료로 건강하고 맛있는 빵을 만듭니다.       </li>
                     </ul>
                 </div>
-                <div className="intro2">
-                    {/* 이미지2 */}
-                    <img src="../../images/intro2.jpg" width='50%' />
+                <div className="about">
+                    <div className="contant2">
+                        verte
+                        <br />
+                        녹색
+                    </div>
+                    <div className="intro2">
+                        {/* 이미지2 */}
+                        <img src="../../images/intro2.jpg" width='50%' />
+                    </div>
                 </div>
-            </div>
 
         </>
     );
