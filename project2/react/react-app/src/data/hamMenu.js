2b2c3f741def26f85151a46f8eafd757fac10d02
kwonyounghye <kyh90721@gmail.com>
2024-01-14 10:24:54 +0900

    0114

--- project2/react/react-app/src/data/hamMenu.js
+++ project2/react/react-app/src/data/hamMenu.js
@@ -2,6 +2,6 @@
 export const hamMenu = {
     // "main":["ABOUT","MENU","CONTACT"],
     "about":['https://scontent.cdninstagram.com/v/t39.30808-6/409629796_18092176408391616_3784383259677904729_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=t9SlwEPthLkAX_iK88k&edm=APs17CUAAAAA&ccb=7-5&oh=00_AfB7zERPYfy3ugP6FAyUoTpR2zKif-Ji2W9k-nz6lokyKQ&oe=65A3E457&_nc_sid=10d13b'],
-    "menu":['../images/menu8.jpg'],
-    "contact":['../images/about7.jpg']
+    "menu":['./images/menu8.jpg'],
+    "contact":['./images/about7.jpg']
 }; //////////// hamMenu //////////////////
